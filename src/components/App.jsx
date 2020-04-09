870fc9254c76b6c2eee49a2c9b9c9f85e44be068
Timofey <Teamoha56@mail.ru>
2020-04-09 12:59:10 +0500

    Добавлены роуты

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import Header from './Header/Header';
 import Navigation from './Navigation/Navigation';
 import ProfilePage from './ProfilePage/ProfilePage';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Dialogs from './Dialogs/Dialogs';
 
 const App = () => {
 	return (
-		<div className="social-network">
-			<Header />
-			<div className="social-network__container">
-				<Navigation />
-				<div className="social-network__content">
-					<ProfilePage />
+		<BrowserRouter>
+			<div className="social-network">
+				<Header />
+				<div className="social-network__container">
+					<Navigation />
+					<div className="social-network__content">
+						<Route path="/profile" component={ProfilePage}/>
+						<Route path="/dialogs" component={Dialogs}/>
+					</div>
 				</div>
 			</div>
-
-		</div>
+		</BrowserRouter>
 	)
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
 	return (
 		<nav className="main-navigation">
 			<ul className="main-navigation__list">
 				<li className="main-navigation__item">
-					<a className="main-navigation__link" href="#">Новости</a>
+					<NavLink className="main-navigation__link" to="/news" activeClassName="main-navigation__link_active">Новости</NavLink>
 				</li>
 
 				<li className="main-navigation__item">
-					<a className="main-navigation__link" href="#">Мой профиль</a>
+					<NavLink className="main-navigation__link" to="/profile" activeClassName="main-navigation__link_active">Мой профиль</NavLink>
 				</li>
 
 				<li className="main-navigation__item">
-					<a className="main-navigation__link" href="#">Мои сообщения</a>
+					<NavLink className="main-navigation__link" to="/dialogs" activeClassName="main-navigation__link_active">Мои сообщения</NavLink>
 				</li>
 
 				<li className="main-navigation__item">
-					<a className="main-navigation__link" href="#">Моя музыка</a>
+					<NavLink className="main-navigation__link" to="/music" activeClassName="main-navigation__link_active">Моя музыка</NavLink>
 				</li>
 
 				<li className="main-navigation__item">
-					<a className="main-navigation__link" href="#">Мои фотографии</a>
+					<NavLink className="main-navigation__link" to="/photos" activeClassName="main-navigation__link_active">Мои фотографии</NavLink>
 				</li>
 			</ul>
 		</nav>
